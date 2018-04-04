92d94a3199af6a3e8cac69c60af34996cd778aee
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:49:07 -0700

    albus: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627 | AOSP
    Test: lunch  [see entries in list]
    
    Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_albus.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_albus-user \
+    lineage_albus-userdebug \
+    lineage_albus-eng
