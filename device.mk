0da1962a23a56a9b85e1c026a81fca45cc2138af
Nolen Johnson <johnsonnolen@gmail.com>
2022-06-18 18:31:01 -0400

    albus: Rename privapp permissions to QTI to reflect what it is
    Change-Id: I5504733e61b44df97eb8f55a9f71121a5a382875

--- device.mk
+++ device.mk
@@ -338,7 +338,7 @@ PRODUCT_PACKAGES += \
 
 # Qualcomm
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-albus.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-albus.xml \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
 # RIL
