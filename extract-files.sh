LineageOS/android_device_motorola_addison

albus: Add a check before adding dependencies
Without a check, when performing a vendor blob regen it'll add another
dependency on top of any existing one

Change-Id: I9c7f17082fd850235c794d44cec3cda0c4b883ef

extract-files.sh
@@ -68,8 +68,10 @@ function blob_fixup() {
 
     # Fix missing symbols
     vendor/lib64/libril-qc-qmi-1.so)
-        patchelf --add-needed "libcutils_shim.so" "${2}"
-        ;;
+        for  LIBCUTILS_SHIM in $(grep -L "libcutils_shim.so" "${2}"); do
+            "${PATCHELF}" --add-needed "libcutils_shim.so" "$LIBCUTILS_SHIM"
+        done
+    ;;
 
     # Fix xml version
     product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)

LineageOS/android_device_motorola_albus

extract-files.sh
@@ -60,7 +60,9 @@ function blob_fixup() {
     
     # Load wrapped shim
     vendor/lib64/libmdmcutback.so)
-        "${PATCHELF}" --add-needed "libqsap_shim.so" "${2}"
+        for LIBQSAP_SHIM in $(grep -L "libqsapshim.so" "${2}"); do
+            "${PATCHELF}" --add-needed "libqsapshim.so" "$LIBQSAP_SHIM"
+        done
         ;;
           
     # Fix thermal engine config path